ed73fb3536068b350a0a42d31ded28ef344af331
hiagogouveia <hiago.gouveia@gmail.com>
2024-05-29 14:05:25 -0400

    concluindo sorteador de numeros

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -3,15 +3,38 @@ function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+    let resultado = document.getElementById("resultado");
     // alert(`O número aleatório foi ${numero}`);
     let sorteados = [];
     let numero;
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
         sorteados.push(numero);
     }
-    alert(`O número aleatório foi ${sorteados}`);
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`; //copiei essa label do index.html
+    alterarStatusBotaoReiniciar();
 }
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function alterarStatusBotaoReiniciar() {
+    let botao = document.getElementById("btn-reiniciar");
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }
+    else {
+        botao.classList.add('container__botao');
+        botao.classList.remove('container__botao-desabilitado');
+    }
+}
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora </label>';
+    alterarStatusBotaoReiniciar();
 }
--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.ts
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.ts
@@ -3,6 +3,7 @@ function sortear(): void {
     let quantidade: number = parseInt((document.getElementById('quantidade') as HTMLInputElement).value);
     let de: number = parseInt((document.getElementById('de') as HTMLInputElement).value);
     let ate: number = parseInt((document.getElementById('ate') as HTMLInputElement).value);
+    let resultado = (document.getElementById("resultado") as HTMLAnchorElement);
     
     // alert(`O número aleatório foi ${numero}`);
     
@@ -11,13 +12,36 @@ function sortear(): void {
 
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate)
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
         sorteados.push(numero);
     }
-    alert(`O número aleatório foi ${sorteados}`);
-
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`; //copiei essa label do index.html
+    alterarStatusBotaoReiniciar();
 }
   
-
 function obterNumeroAleatorio(min: number, max: number){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotaoReiniciar(){
+    let botao = (document.getElementById("btn-reiniciar") as HTMLAnchorElement);
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.add('container__botao');
+        botao.classList.remove('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    (document.getElementById('quantidade') as HTMLInputElement).value = '';
+    (document.getElementById('de') as HTMLInputElement).value = '';
+    (document.getElementById('ate') as HTMLInputElement).value = '';
+    (document.getElementById('resultado') as HTMLInputElement).innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora </label>';
+    
+    alterarStatusBotaoReiniciar()
 }
