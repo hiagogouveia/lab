f411c50be7fa3b4da30d1b8bca2109ed0a43c711
hiagogouveia <hiago.gouveia@gmail.com>
2024-06-06 15:15:14 -0400

    elfo games

--- 3411-praticando-logica-projeto_inicial/alugames/js/app.ts
+++ 3411-praticando-logica-projeto_inicial/alugames/js/app.ts
@@ -1,17 +1,18 @@
-"use strict";
-function alterarStatus(id) {
-    let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('.dashboard__item__img');
-    let botao = gameClicado.querySelector('.dashboard__item__button');
+function alterarStatus(id: string): void {
+    let gameClicado: HTMLElement = document.getElementById(`game-${id}`);
+    let imagem: HTMLImageElement = gameClicado.querySelector('.dashboard__item__img');
+    let botao: HTMLButtonElement = gameClicado.querySelector('.dashboard__item__button');
+    
+
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         alert("para devolver o jogo, clique em OK");
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = "Alugar";
-    }
-    else {
+    }else{
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = "Devolver";
     }
+
 }
